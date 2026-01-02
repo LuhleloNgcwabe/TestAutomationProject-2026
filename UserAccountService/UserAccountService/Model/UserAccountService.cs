b3e05245a965a8c051ee5cfb699ce8cc53bde415
Luhlelo Ngcwabe <Dngcwabe@gmail.com>
2026-01-01 20:34:06 -0800

    Add user account service project

---
+++ UserAccountService/UserAccountService/Model/UserAccountService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class UserService
+{
+    private Dictionary<string, string> users = new();
+
+    public void Register(string username, string password)
+    {
+        if (string.IsNullOrEmpty(username))
+            throw new ArgumentException("Username cannot be empty");
+        if (password == null || password.Length < 6)
+            throw new ArgumentException("Password too short");
+        users[username] = password;
+    }
+
+    public bool Login(string username, string password)
+    {
+        if (!users.ContainsKey(username))
+            throw new Exception("User not registered");
+        return users[username] == password;
+    }
+
+    public void ResetPassword(string username, string newPassword)
+    {
+        if (!users.ContainsKey(username))
+            throw new Exception("User not found");
+        users[username] = newPassword;
+    }
+}
+
+
