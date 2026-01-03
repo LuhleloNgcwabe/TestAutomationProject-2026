40c530fe2d4de5efa242b2bf0d2ce5ba92053906
Luhlelo Ngcwabe <Dngcwabe@gmail.com>
2026-01-03 21:41:14 +0200

    First ExtentReport integration

--- NunitPlusExtentReportPractise/NunitPlusExtentReportPractise/BasicMathTests.cs
+++ NunitPlusExtentReportPractise/NunitPlusExtentReportPractise/BasicMathTests.cs
@@ -1,8 +1,24 @@
-﻿namespace NunitPlusExtentReportPractise
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace NunitPlusExtentReportPractise
 {
     [TestFixture]
     public class BasicMathTests
     {
+        public ExtentReports extent;
+
+        [OneTimeSetUp]
+        public void GlobalSetup()
+        {
+            var reporter = new ExtentSparkReporter("ExtentReport.html");
+            extent = new ExtentReports();
+            extent.AttachReporter(reporter);
+
+            TestContext.Out.WriteLine("Initialize extent report");
+        }
+
+
         [SetUp]
         public void Setup()
         {
@@ -10,24 +26,31 @@
             TestContext.Out.WriteLine("Test set up is finished");
         }
 
-        [TestCase(1,2,3)]
-        [TestCase(2, 3,5)]
-        [TestCase(3, 5,8)]
-
+        [TestCase(1, 2, 3)]
+        [TestCase(2, 3, 5)]
+        [TestCase(3, 5, 8)]
         public void Add_TwoNumbers_ReturnsCorrectSum(int a, int b, int answer)
         {
+            extent.CreateTest(TestContext.CurrentContext.Test.Name);
             // Act
             TestContext.Out.WriteLine($"Add {a} and {b}");
             int result = a + b;
             // Assert
             TestContext.Out.WriteLine($"Verify that answer is {answer}!!");
-            Assert.That(answer,Is.EqualTo( result));
+            Assert.That(answer, Is.EqualTo(result));
         }
-
+        
         [TearDown]
         public void TearDown()
         {
             TestContext.Out.WriteLine("Test is completed");
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTearDown()
+        {
+            TestContext.Out.WriteLine("Save all recorded data to a report file");
+            extent.Flush();
         }
     }
 }
