65bdb6adfa55eaa66dc0132db3238d4560500292
Luhlelo Ngcwabe <Dngcwabe@gmail.com>
2026-01-03 14:50:59 +0200

    Create .Net project and add required dependancies

---
+++ NunitPlusExtentReportPractise/NunitPlusExtentReportPractise/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace NunitPlusExtentReportPractise
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
