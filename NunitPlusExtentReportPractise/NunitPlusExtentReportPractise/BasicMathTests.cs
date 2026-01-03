dc447d3f207c85a08a98f00931bb987d8448d61b
Luhlelo Ngcwabe <Dngcwabe@gmail.com>
2026-01-03 15:01:39 +0200

    Create Basic Math TestFixture, with a passing test

---
+++ NunitPlusExtentReportPractise/NunitPlusExtentReportPractise/BasicMathTests.cs
@@ -0,0 +1,27 @@
+﻿namespace NunitPlusExtentReportPractise
+{
+    [TestFixture]
+    public class BasicMathTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            // Arrange
+            int a = 2;
+            int b = 3;
+            int answer = 5;
+
+            // Act
+            int result = a + b;
+
+
+            // Assert
+            Assert.That(answer,Is.EqualTo( result));
+        }
+    }
+}
