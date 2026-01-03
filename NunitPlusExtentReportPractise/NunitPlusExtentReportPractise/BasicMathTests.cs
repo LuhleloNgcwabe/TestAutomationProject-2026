54ac2fb471c9588dfb5f3271fe0520bd51e525e7
Luhlelo Ngcwabe <Dngcwabe@gmail.com>
2026-01-03 15:14:35 +0200

    Introduce test life cycle hooks

--- NunitPlusExtentReportPractise/NunitPlusExtentReportPractise/BasicMathTests.cs
+++ NunitPlusExtentReportPractise/NunitPlusExtentReportPractise/BasicMathTests.cs
@@ -3,25 +3,33 @@
     [TestFixture]
     public class BasicMathTests
     {
+        int a, b, answer;
         [SetUp]
         public void Setup()
         {
+            // Arrange
+            a = 2;
+            b = 3;
+            answer = 5;
+            TestContext.Out.WriteLine("Test set up is finished");
         }
 
         [Test]
         public void Add_TwoNumbers_ReturnsCorrectSum()
         {
-            // Arrange
-            int a = 2;
-            int b = 3;
-            int answer = 5;
-
             // Act
+            TestContext.Out.WriteLine($"Add {a} and {b}");
             int result = a + b;
-
-
             // Assert
+            TestContext.Out.WriteLine($"Verify that answer is {answer}!!");
             Assert.That(answer,Is.EqualTo( result));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            a = b = answer = 0;
+            TestContext.Out.WriteLine("Test is completed");
         }
     }
 }
