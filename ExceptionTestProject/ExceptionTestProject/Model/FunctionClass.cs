bc35df662d16dd6bfa4c75435dfadd6a28528fc5
Luhlelo Ngcwabe <Dngcwabe@gmail.com>
2026-01-12 08:07:59 +0200

    Add Exception Practice project
    - Completed PART 1: Basic Exception Testing (NUnit)

---
+++ ExceptionTestProject/ExceptionTestProject/Model/FunctionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionTestProject.Model
+{
+    public class FunctionClass
+    {
+        public void DoWork(bool isVallid)
+        {
+            if (!isVallid)
+            {
+                throw new InvalidOperationException("Invalid operation. The method call is invalid for the objects current state.");
+            }
+        }
+
+        public void Process(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if (input.Length < 3)
+                throw new ArgumentException("Input too short");
+        }
+            
+    }
+}
---
+++ ExceptionTestProject/ExceptionTestProject/Test/MyTests.cs
@@ -0,0 +1,55 @@
+﻿using ExceptionTestProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ExceptionTestProject.Test
+{
+    public class MyTests
+    {
+        FunctionClass methods;
+        [SetUp]
+        public void initFunctionClass()
+        {
+            methods = new FunctionClass(); 
+        }
+
+        [Test]
+        public void DoWork_WhenInvalid_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => methods.DoWork(false));
+        }
+
+        [Test]
+        public void DoWork_throwException_WithCorrectMessage()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => methods.DoWork(false));
+            Assert.That(ex.Message, Is.EqualTo("Invalid operation. The method call is invalid for the objects current state."));
+        }
+
+        [Test]
+        public void DoWork_WhenValid_DoesNotThrow()
+        {
+            Assert.Ignore("Method is not yet implemented.");
+            Assert.DoesNotThrow(() => methods.DoWork(true));
+        }
+
+        [Test]
+        public void process_WhenNull_throwsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => methods.Process(null));
+        }
+        [Test]
+        public void process_WhenTooShort_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => methods.Process("ab"));
+        }
+        [Test]
+        public void process_WhenGivenCorrectvalue_DoesNotThrow()
+        {
+            Assert.Ignore("this is not part of the exercises");
+            Assert.DoesNotThrow(() => methods.Process("Luhlelo"));
+        }
+    }
+}
